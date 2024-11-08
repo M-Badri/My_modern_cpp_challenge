c505f58ce5964362efa6b776b96b5e1485dbf8f9
Mehdi Badri <mehdi.badri@gmail.com>
2024-11-07 21:54:10 -0500

    draft for P3_V1 and P3_V2

--- src/Chapter01/problem_04/problem_04_v1.cpp
+++ src/Chapter01/problem_04/problem_04_v1.cpp
@@ -2,24 +2,87 @@
 #include <utility>
 #include <vector> 
 
+
+
 int main() {
 
   using namespace std;
 
-  int a, b; 
-  cout << "Enter the number " << endl;
+  int a, LPN; 
+  cout << "Enter the number: " << endl;
   cin >> a;
 
-  int LCM = a*b;
-  for ( int i{a}; i < a*b; ++i){
-    if ((i % a == 0) & (i % b == 0)){
-      LCM = i;
-      break;
-    }
+  vector<pair<int, bool>> allData;
+
+  for (auto i{2}; i<a; i++ ){
+    allData.push_back(make_pair(i, true));
   }
 
-  string formated_string = format("The LCM between {} and {} is: {}", a, b, LCM);
-  cout << formated_string << endl;
+  for (auto i{0}; auto data : allData){
+    if(data.first){
+      for (auto j = (data.first*data.first)-2; j<a-2; j++){
+        if(allData[j].first % data.first == 0){
+          allData[j].second = false;
+        }
+      }
+    }
+    i++;
+  }
+
+  for (auto i{0}; auto data : allData){
+    cout << data.first << " " << data.second << endl; 
+  }
 
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // vector<int> data;
+  // vector<bool> flag(a, true);
+
+  // for (auto i{0}; i<a; ++i){
+  //   data.push_back(i+1);
+  // }
+
+  // for (auto i{1}; i<a; ++i){
+  //   if (flag[i]){
+  //     for (auto j=data[i]*data[i]; j<a; ++j ){
+  //       if(j%data[i]==0){
+  //         flag[j-1]=false;
+  //       }
+  //     }
+  //   }
+  // }
+
+
+  //  for (auto i{0}; i<a-1; i++){
+  //   cout << data[i] << " " << flag[i] << endl;
+    // flag[i] = false;
+  // }
+
+  
+
+  // string formated_string = format("The largest prime number smaller than {} is: {}", a, LPN);
+  // cout << formated_string << endl;
+
