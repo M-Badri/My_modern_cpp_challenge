b6a8bc385af6a3ed56fdd211a071dad658e8a1d5
Mehdi Badri <mehdi.badri@gmail.com>
2024-11-08 22:12:09 -0500

    complete P4_V1.

--- src/Chapter01/problem_04/problem_04_v1.cpp
+++ src/Chapter01/problem_04/problem_04_v1.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <utility>
 #include <vector> 
-#include <algorithm>
-
-
 
 int main() {
 
@@ -14,7 +10,6 @@ int main() {
   cin >> a;
 
   vector<pair<int, bool>> dataList;
-
   for (auto i{2}; i<a; i++ ){
     dataList.push_back(make_pair(i, true));
   }
@@ -32,74 +27,11 @@ int main() {
 
   auto LPN{0};
   for (auto i{0}; auto data : dataList){
-    // cout << data.first << " " << data.second << endl; 
     if(data.second){if(data.first > LPN){LPN=data.first;};}
   }
-  cout << LPN << "+++++++++++" << endl;
 
-
-
-  auto max_it = std::max_element(dataList.begin(), dataList.end(),
-                [](const std::pair<int, bool>& a, const std::pair<int, bool>& b){
-                  if (!a.second) return true;
-                  if (!b.second) return false;
-                  return a.first < b.first;
-                }        
+  auto frmtd_str = format("The largest prime number smaller than {} is: {}", a, LPN);
+  cout << "\n" << frmtd_str << endl;
   
-  );
-
-  cout << max_it->first << "**********" << endl;
-
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // vector<int> data;
-  // vector<bool> flag(a, true);
-
-  // for (auto i{0}; i<a; ++i){
-  //   data.push_back(i+1);
-  // }
-
-  // for (auto i{1}; i<a; ++i){
-  //   if (flag[i]){
-  //     for (auto j=data[i]*data[i]; j<a; ++j ){
-  //       if(j%data[i]==0){
-  //         flag[j-1]=false;
-  //       }
-  //     }
-  //   }
-  // }
-
-
-  //  for (auto i{0}; i<a-1; i++){
-  //   cout << data[i] << " " << flag[i] << endl;
-    // flag[i] = false;
-  // }
-
-  
-
-  // string formated_string = format("The largest prime number smaller than {} is: {}", a, LPN);
-  // cout << formated_string << endl;
-
